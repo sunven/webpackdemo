764897ca758cbb8e6a6f5591d79b411afb3305a2
孙文权 <604164227@qq.com>
2018-05-31 18:52:47 +0800

    style-loader

--- app/main.js
+++ app/main.js
@@ -1,2 +1,6 @@
 const greeter = require('./add.js');
+// import filecss from './css/file.css'
+// console.log(filecss);
+const css = require('./css/file.css').toString();
+console.log(css);
 document.querySelector("#app").appendChild(greeter());
--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,23 @@
 module.exports = {
 	devtool: 'eval-source-map',
-	module: {},
-
+	module: {
+		rules: [{
+			test: /\.css$/,
+			use: [
+				'css-loader'
+			]
+		}]
+	},
 	plugins: [],
 
-	entry: __dirname + "/app/main.js", //已多次提及的唯一入口文件
+	entry: './app/main.js',
 	output: {
 		path: __dirname + "/public",
 		filename: "bundle.js"
 	},
 	devServer: {
-		contentBase: "./public",//本地服务器所加载的页面所在的目录
-		historyApiFallback: true,//不跳转
-		inline: true//实时刷新
-	  }
+		contentBase: "./public", //本地服务器所加载的页面所在的目录
+		historyApiFallback: true, //不跳转
+		inline: true //实时刷新
+	}
 };
