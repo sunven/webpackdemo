ab57a9903511fb258f5e2e17e4f1ed7bb093577b
孙文权 <604164227@qq.com>
2018-09-21 18:13:59 +0800

    add uglifyjs demo

--- app/main.js
+++ app/main.js
@@ -1,4 +1,5 @@
-const greeter = require('./add.js');
-// import img from './image/logo.png'
-import filecss from './css/file.css'
-document.querySelector("#app").appendChild(greeter());
+var UglifyJS = require("uglify-js");
+var code = "function add(first, second) { return first + second; }";
+var result = UglifyJS.minify(code);
+console.log(result.error); // runtime error, or `undefined` if no error
+console.log(result.code);  // minified output: function add(n,d){return n+d}
